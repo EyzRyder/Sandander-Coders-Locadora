bcb4cdb6b60e5f00dfaf073fec41ae35a315122b
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-11-04 23:18:31 -0300

    chore: add controller and requestmapping anotation to contollers

--- src/main/java/com/ada/santander/coders/locadora/controller/ClienteController.java
+++ src/main/java/com/ada/santander/coders/locadora/controller/ClienteController.java
@@ -1,4 +1,9 @@
 package com.ada.santander.coders.locadora.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
 }
--- src/main/java/com/ada/santander/coders/locadora/controller/VeiculoController.java
+++ src/main/java/com/ada/santander/coders/locadora/controller/VeiculoController.java
@@ -1,4 +1,15 @@
 package com.ada.santander.coders.locadora.controller;
 
+import com.ada.santander.coders.locadora.service.VeiculoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/veiculo")
 public class VeiculoController {
+    private VeiculoService veiculoService;
+
+    public VeiculoController(VeiculoService veiculoService) {
+        this.veiculoService = veiculoService;
+    }
 }
