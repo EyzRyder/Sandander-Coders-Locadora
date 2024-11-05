76e663e072147e00d314c8d9d31b3e88df8559d6
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-11-04 22:23:09 -0300

    initialize spring

---
+++ src/test/java/com/ada/santander/coders/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.santander.coders.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
