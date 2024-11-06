3ae4c5d680e833eebc4b4d37f925eccabe7684d1
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-11-05 21:17:26 -0300

    feat: implementação do controlador de Cliente com endpoint para buscar por id

--- src/main/java/com/ada/santander/coders/locadora/controller/ClienteController.java
+++ src/main/java/com/ada/santander/coders/locadora/controller/ClienteController.java
@@ -1,9 +1,32 @@
 package com.ada.santander.coders.locadora.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ada.santander.coders.locadora.entity.Cliente;
+import com.ada.santander.coders.locadora.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+
+    @GetMapping
+    public List<Cliente> listarTodos(){
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+        return clienteService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
--- src/main/java/com/ada/santander/coders/locadora/service/ClienteService.java
+++ src/main/java/com/ada/santander/coders/locadora/service/ClienteService.java
@@ -1,7 +1,43 @@
 package com.ada.santander.coders.locadora.service;
 
+import com.ada.santander.coders.locadora.entity.Cliente;
+import com.ada.santander.coders.locadora.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> listarTodos(){
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id){
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente salvar(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente atualizar(Long id, Cliente clienteAtualizado){
+        return clienteRepository.findById(id)
+                .map(cliente -> {
+                    cliente.setNome(clienteAtualizado.getNome());
+                    cliente.setCpf(clienteAtualizado.getCpf());
+                    cliente.setEmail(clienteAtualizado.getEmail());
+                    cliente.setTelefone(clienteAtualizado.getTelefone());
+
+                    return clienteRepository.save(cliente);
+                })
+
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
 }
