a7e5451e26999d6b5e96bee519bc14e0f1c740ef
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-11-07 00:26:43 -0300

    refacter: add autowired to service

--- src/main/java/com/ada/santander/coders/locadora/service/VeiculoService.java
+++ src/main/java/com/ada/santander/coders/locadora/service/VeiculoService.java
@@ -2,14 +2,13 @@ package com.ada.santander.coders.locadora.service;
 
 import com.ada.santander.coders.locadora.repository.VeiculoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VeiculoService {
-    private VeiculoRepository veiculoRepository;
 
-    public VeiculoService(VeiculoRepository veiculoRepository) {
-        this.veiculoRepository = veiculoRepository;
-    }
+    @Autowired
+    private VeiculoRepository veiculoRepository;
 
 }
