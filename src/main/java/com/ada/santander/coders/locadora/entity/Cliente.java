6e16bab900564eb59ccb10ded43d385fb389e505
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-11-08 08:38:17 -0300

    feat: adicionando validação no campo nome, email, cpf e telefone

--- src/main/java/com/ada/santander/coders/locadora/entity/Cliente.java
+++ src/main/java/com/ada/santander/coders/locadora/entity/Cliente.java
@@ -1,16 +1,14 @@
 package com.ada.santander.coders.locadora.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "CLIENTE")
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/com/ada/santander/coders/locadora/service/ClienteService.java
+++ src/main/java/com/ada/santander/coders/locadora/service/ClienteService.java
@@ -4,15 +4,21 @@ import com.ada.santander.coders.locadora.entity.Cliente;
 import com.ada.santander.coders.locadora.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Service
 public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$");
+
 
     public List<Cliente> listarTodos(){
         return clienteRepository.findAll();
@@ -42,6 +48,28 @@ public class ClienteService {
 
     public void deletar(Long id){
         clienteRepository.deleteById(id);
+    }
+
+    private void validarCliente(Cliente cliente){
+        if (!StringUtils.hasLength(cliente.getNome()) || cliente.getNome().length() <= 3) {
+            throw new IllegalArgumentException("O nome deve ter mais de 3 caracteres!");
+        }
+
+
+        if (!cliente.getCpf().matches("\\d{11}")) {
+            throw new IllegalArgumentException("O CPF deve ter 11 dígitos numéricos!");
+        }
+
+
+
+        if (!EMAIL_PATTERN.matcher(cliente.getEmail()).matches()) {
+            throw new IllegalArgumentException("O e-mail está em um formato inválido!");
+        }
+
+
+        if (!cliente.getTelefone().matches("\\d{8,15}")) {
+            throw new IllegalArgumentException("O telefone deve ter entre 8 e 15 dígitos numéricos!");
+        }
     }
 
 }
