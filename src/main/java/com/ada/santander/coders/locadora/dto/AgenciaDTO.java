3d6a55d5401c993b80e18d1f8112a0ac7287776d
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-11-09 15:38:24 -0300

    chore: remove unused import statement

--- src/main/java/com/ada/santander/coders/locadora/dto/AgenciaDTO.java
+++ src/main/java/com/ada/santander/coders/locadora/dto/AgenciaDTO.java
@@ -1,9 +1,6 @@
 package com.ada.santander.coders.locadora.dto;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
