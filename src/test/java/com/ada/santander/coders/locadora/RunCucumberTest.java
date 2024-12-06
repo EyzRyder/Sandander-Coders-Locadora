08b6baf4ec61c05fb8fee34d3819738073d6547b
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-12-06 16:30:00 -0300

    feat: add cucumber runnner

---
+++ src/test/java/com/ada/santander/coders/locadora/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package com.ada.santander.coders.locadora;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features"
+)
+public class RunCucumberTest {
+}
